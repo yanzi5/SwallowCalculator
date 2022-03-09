aa3f7a467e90f2f7702ef2bcd44ae46fe4dbc0d8
yanzi5 <1907535456@qq.com>
2022-03-09 15:16:58 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(180, 209)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(106, 50)
+        Me.Button19.TabIndex = 39
+        Me.Button19.Text = "="
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(236, 41)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(50, 50)
+        Me.Button18.TabIndex = 38
+        Me.Button18.Text = "退出"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(180, 41)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(50, 50)
+        Me.Button17.TabIndex = 37
+        Me.Button17.Text = "ON"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(236, 97)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(50, 50)
+        Me.Button16.TabIndex = 36
+        Me.Button16.Text = "/"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(180, 97)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(50, 50)
+        Me.Button15.TabIndex = 35
+        Me.Button15.Text = "*"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(236, 153)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(50, 50)
+        Me.Button14.TabIndex = 34
+        Me.Button14.Text = "-"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(180, 153)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(50, 50)
+        Me.Button13.TabIndex = 33
+        Me.Button13.Text = "+"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(124, 209)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(50, 50)
+        Me.Button12.TabIndex = 32
+        Me.Button12.Text = "归零"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(68, 209)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(50, 50)
+        Me.Button11.TabIndex = 31
+        Me.Button11.Text = "."
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(12, 209)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(50, 50)
+        Me.Button10.TabIndex = 30
+        Me.Button10.Text = "0"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(124, 41)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(50, 50)
+        Me.Button9.TabIndex = 29
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(68, 41)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(50, 50)
+        Me.Button8.TabIndex = 28
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 41)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(50, 50)
+        Me.Button7.TabIndex = 27
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(124, 97)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(50, 50)
+        Me.Button6.TabIndex = 26
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(68, 97)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(50, 50)
+        Me.Button5.TabIndex = 25
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 97)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(50, 50)
+        Me.Button4.TabIndex = 24
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(124, 153)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(50, 50)
+        Me.Button3.TabIndex = 23
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(68, 153)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(50, 50)
+        Me.Button2.TabIndex = 22
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 153)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(50, 50)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(274, 23)
+        Me.TextBox1.TabIndex = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(296, 271)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,342 @@
+﻿Public Class Form1
+    '声明全局变量
+    Dim strmiddle() As String = {"0", "0", "0"} '数组型
+    Dim calmethod1 As String = "0" '文本型
+    Dim calmethod2 As String = "0" '文本型
+    Dim strdot As Boolean = False '逻辑型
+
+    '按钮[0]
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If strmiddle(0) = "0" Then
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & “0”
+            TextBox1.Text = strmiddle(0) & “.”
+        Else
+            strmiddle(0) = strmiddle(0) & “0”
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[.]
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        strdot = True
+        strmiddle(0) = strmiddle(0) & “.”
+        TextBox1.Text = strmiddle(0)
+    End Sub
+
+    '按钮[归零]
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        strmiddle(0) = 0 '将变量设置为0
+        strmiddle(1) = 0
+        strmiddle(2) = 0
+        calmethod1 = 0
+        calmethod2 = 0
+        strdot = False
+        TextBox1.Text = “0”
+    End Sub
+
+    '按钮[=]
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        If strmiddle(2) = "0" Then
+            Select Case calmethod1
+                Case "+"
+                    TextBox1.Text = Str(Val(strmiddle(1)) + Val(strmiddle(0)))
+                Case "-"
+                    TextBox1.Text = Str(Val(strmiddle(1)) - Val(strmiddle(0)))
+                Case "*"
+                    TextBox1.Text = Str(Val(strmiddle(1)) * Val(strmiddle(0)))
+                Case "/"
+                    If strmiddle(0) = "0" Then
+                        TextBox1.Text = "错误！"
+                    Else
+                        TextBox1.Text = Str(Val(strmiddle(1)) / Val(strmiddle(0)))
+                    End If
+            End Select
+
+        ElseIf calmethod2 = “*” Then
+            strmiddle(0) = Str(Val(strmiddle(0)) * Val(strmiddle(2)))
+            Select Case calmethod1
+                Case "+"
+                    TextBox1.Text = Str(Val(strmiddle(1)) + Val(strmiddle(0)))
+                Case "-"
+                    TextBox1.Text = Str(Val(strmiddle(1)) - Val(strmiddle(0)))
+                Case "*"
+                    TextBox1.Text = Str(Val(strmiddle(1)) * Val(strmiddle(0)))
+                Case "/"
+                    If strmiddle(0) = "0" Then
+                        TextBox1.Text = "错误！"
+                    Else
+                        TextBox1.Text = Str(Val(strmiddle(1)) / Val(strmiddle(0)))
+                    End If
+            End Select
+
+        ElseIf calmethod2 = “/” Then
+            strmiddle(0) = Str(Val(strmiddle(2)) * Val(strmiddle(0)))
+            Select Case calmethod1
+                Case "+"
+                    TextBox1.Text = Str(Val(strmiddle(1)) + Val(strmiddle(0)))
+                Case "-"
+                    TextBox1.Text = Str(Val(strmiddle(1)) - Val(strmiddle(0)))
+                Case "*"
+                    TextBox1.Text = Str(Val(strmiddle(1)) * Val(strmiddle(0)))
+                Case "/"
+                    If strmiddle(0) = "0" Then
+                        TextBox1.Text = "错误！"
+                    Else
+                        TextBox1.Text = Str(Val(strmiddle(1)) / Val(strmiddle(0)))
+                    End If
+            End Select
+        End If
+
+    End Sub
+
+    '按钮[1]
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "1"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "1"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "1"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[2]
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "2"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "2"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "2"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[3]
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "3"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "3"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "3"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[4]
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "4"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "4"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "4"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[5]
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "5"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "5"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "5"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[6]
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "6"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "6"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "6"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[7]
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "7"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "7"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "7"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[8]
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "8"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "8"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "8"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[9]
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If strmiddle(0) = "0" Then
+            strmiddle(0) = "9"
+            TextBox1.Text = strmiddle(0) & "."
+        ElseIf strdot = False Then
+            strmiddle(0) = strmiddle(0) & "9"
+            TextBox1.Text = strmiddle(0) & "."
+        Else
+            strmiddle(0) = strmiddle(0) & "9"
+            TextBox1.Text = strmiddle(0)
+        End If
+    End Sub
+
+    '按钮[+]
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If calmethod1 = "0" Then
+            calmethod1 = "+"
+            strmiddle(1) = strmiddle(0)
+            strmiddle(0) = "0"
+        Else : Select Case calmethod1
+                Case "+"
+                    strmiddle(1) = Str(Val(strmiddle(0)) + Val(strmiddle(1)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "+"
+                Case "-"
+                    strmiddle(1) = Str(Val(strmiddle(1)) - Val(strmiddle(0)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "+"
+                Case "*"
+                    strmiddle(1) = Str(Val(strmiddle(0)) * Val(strmiddle(1)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "+"
+                Case "/"
+                    strmiddle(1) = Str(Val(strmiddle(1)) / Val(strmiddle(0)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "+"
+            End Select
+        End If
+    End Sub
+
+    '按钮[-]
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If calmethod1 = "0" Then
+            calmethod1 = "-"
+            strmiddle(1) = strmiddle(0)
+            strmiddle(0) = "0"
+        Else : Select Case calmethod1
+                Case "+"
+                    strmiddle(1) = Str(Val(strmiddle(0)) + Val(strmiddle(1)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "-"
+                Case "-"
+                    strmiddle(1) = Str(Val(strmiddle(1)) - Val(strmiddle(0)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "-"
+                Case "*"
+                    strmiddle(1) = Str(Val(strmiddle(0)) * Val(strmiddle(1)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "-"
+                Case "/"
+                    strmiddle(1) = Str(Val(strmiddle(1)) / Val(strmiddle(0)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "-"
+            End Select
+        End If
+    End Sub
+
+    '按钮[*]
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        If calmethod1 = "0" Then
+            calmethod1 = "*"
+            strmiddle(1) = strmiddle(0)
+            strmiddle(0) = "0"
+        Else : Select Case calmethod1
+                Case "+"
+                    calmethod2 = "*"
+                    strmiddle(2) = strmiddle(0)
+                    strmiddle(0) = "0"
+                Case "-"
+                    calmethod2 = "*"
+                    strmiddle(2) = strmiddle(0)
+                    strmiddle(0) = "0"
+                Case "*"
+                    strmiddle(1) = Str(Val(strmiddle(0)) * Val(strmiddle(1)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "*"
+                Case "/"
+                    strmiddle(1) = Str(Val(strmiddle(1)) / Val(strmiddle(0)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "*"
+            End Select
+        End If
+    End Sub
+
+    '按钮[/]
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        If calmethod1 = "0" Then
+            calmethod1 = "/"
+            strmiddle(1) = strmiddle(0)
+            strmiddle(0) = "0"
+        Else : Select Case calmethod1
+                Case "+"
+                    calmethod2 = "/"
+                    strmiddle(2) = strmiddle(0)
+                    strmiddle(0) = "0"
+                Case "-"
+                    calmethod2 = "/"
+                    strmiddle(2) = strmiddle(0)
+                    strmiddle(0) = "0"
+                Case "*"
+                    strmiddle(1) = Str(Val(strmiddle(0)) * Val(strmiddle(1)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "/"
+                Case "/"
+                    strmiddle(1) = Str(Val(strmiddle(1)) / Val(strmiddle(0)))
+                    strmiddle(0) = "0"
+                    calmethod1 = "/"
+            End Select
+        End If
+    End Sub
+
+    '按钮[ON]
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        TextBox1.Text = "0"
+    End Sub
+
+    '按钮[退出]
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        Me.Close() '结束软件
+    End Sub
+
+    '窗口创建时
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
